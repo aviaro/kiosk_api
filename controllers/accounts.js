6b78a46666dc9455fe037b3e91526ce83ae2da3c
aviaro <aviarb@outlook.com>
2022-03-14 10:28:20 +0200

    added functions

--- controllers/accounts.js
+++ controllers/accounts.js
@@ -3,11 +3,63 @@ const mongoose = require('mongoose');
 const user = require('../models/user');
 const router = express.Router();
 const bcryptjs = require('bcryptjs');
+const jwt  = require('jsonwebtoken');
 
 //MODELS 
 const User = require('../models/user')
-
+//login 
+router.post("/login", async (request, response) => {
+    const { email, password } = request.body;
+    User.findOne({ email: email })
+      .then(async (user) => {
+        if (user) {
+          if (user.isApproved && !user.isLocked) {
+            const isMatch = await bcryptjs.compare(password, user.password);
+            if (isMatch) {
+              //create token
+              const acc_data = {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                avata: user.avata,
+                mobile: user.mobile,
+                email: user.email,
+                _id: user._id,
+              };
+  
+              const token = await jwt.sign(
+                acc_data,
+                "A6cXZ9Mj5hM4As2wiIugIz5DHNO3q1VF"
+              );
+  
+              // response
+              return response.status(200).json({
+                msg: token,
+              });
+            } else {
+              return response.status(200).json({
+                msg: "your password is not match",
+              });
+            }
+          } else {
+            return response.status(200).json({
+              msg: "your account is not approved",
+            });
+          }
+        } else {
+          return response.status(200).json({
+            msg: "email not exist",
+          });
+        }
+      })
+      .catch((err) => {
+        return response.status(200).json({
+          msg: "user not found",
+        });
+      });
+  });
+// create account
 router.post('/createAccount',async(request, response) => {
+
     //  get user inputs
     const {firstname,lastname,email,password,mobile} = request.body;
     //  check if user exists
@@ -65,8 +117,41 @@ router.post('/createAccount',async(request, response) => {
     // Response  from server
 
 })
-
-
+// verify the pascode
+router.post("/verify", async (request, response) => {
+    // get passcode and email
+    const { email, passcode } = request.body;
+    // is user exists
+    User.findOne({ email })
+      .then(async (user) => {
+        // verify code
+        if (user) {
+          if (user.passcode == passcode) {
+            // update isApproved
+            user.isApproved = true;
+            user.save().then((accountUpdate) => {
+              return response.status(200).json({
+                msg: accountUpdate,
+              });
+            });
+          } else {
+            return response.status(200).json({
+              msg: "passcode not match",
+            });
+          }
+        } else {
+          // response
+          return response.status(200).json({
+            msg: "user not found",
+          });
+        }
+      })
+      .catch((err) => {
+        return response.status(500).json({
+          msg: err,
+        });
+      });
+  });
 router.get('/sayhello',async(req,res) =>{
     try {
         const users = await User.find()
