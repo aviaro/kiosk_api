36a52634636acf8714652b3339cec6b7e2c5f077
aviaro <aviarb@outlook.com>
2022-04-04 19:32:56 +0300

    04/04/2022

--- controllers/product.js
+++ controllers/product.js
@@ -2,128 +2,155 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const isAuth = require('./isAuth');
-
 const User = require('../models/user');
 const Store = require('../models/store');
 const Category = require('../models/category');
 const Product = require('../models/product');
-
-
-
 router.post('/addCategory', isAuth, async(request,response) => {
-    const userid= request.account._id;
-    const store = await Store.findOne({associateId:userid})    
-    const{
-        categoryName,
-        categoryImage, 
-        priority      
-    } = request.body;
-
+    const accountId = request.account._id;
+    const store = await Store.findOne({associateId: accountId});
+    const categoryId = mongoose.Types.ObjectId();
+    const { categoryName, categoryImage, priority } = request.body;
     const _category = new Category({
-        _id: mongoose.Types.ObjectId(),
+        _id: categoryId,
         storeId: store._id,
         categoryName: categoryName,
         categoryImage: categoryImage,
         priority: priority
-    })
-    return _category.save()
-    .then(newCategory => {
+    });
+    _category.save()
+    .then(category_created => {
         return response.status(200).json({
-            category: newCategory
-        })
+            status: true,
+            message: category_created
+        });
     })
     .catch(err => {
-        response.status(500).json({
+        return response.status(500).json({
+            status: false,
             message: err
-        })
+        });
     })
-
 });
-router.put('/updateCategory/:categoryId', isAuth, async(request,response) => { 
-    const{
-        categoryName,
-        categoryImage, 
-        priority      
-    } = request.body;
+router.put('/updateCategory/:categoryId', isAuth, async(request,response) => {
+
+    const { categoryName, categoryImage, priority } = request.body;
     const cid = request.params.categoryId;
-    await Category.findById(cid)
+
+    Category.findById(cid)
     .then(category => {
-        if(category)
-        {
-            category.categoryName=categoryName;
-            category.categoryImage=categoryImage;
-            category.priority=priority;
-            category.save()
+        if(category){
+            category.categoryName = categoryName;
+            category.categoryImage = categoryImage;
+            category.priority = priority;
             return category.save()
-            .then(categoryupdated => {
+            .then(category_updated => {
                 return response.status(200).json({
                     status: true,
-                    message: categoryupdated
+                    message: category_updated
                 });
-        
             })
-        }
-        else
-        {
-            return response.status(403).json({
+        } else {
+            return response.status(200).json({
                 status: false,
-                message:'category not found'
+                message: 'Category not found'
             });
-
         }
     })
     .catch(err => {
         return response.status(500).json({
             status: false,
-            error:err
-        })
+            message: err
+        });
     })
-   
-    
-    
-
 });
-router.delete('/deleteCategory', isAuth, async(request,response) => { 
-
-
-});
-router.post('/addProduct', isAuth, async(request,response) => { });
-router.put('/updateProduct', isAuth, async(request,response) => { });
-router.delete('/deleteProduct', isAuth, async(request,response) => {
-    
- });
-router.post('/getallcategories', isAuth, async(request,response) => {
-    const accountId = request.account._id;
-    const store = await Store.findOne({associateId: accountId})
-    category.find({storeId: store._id})
-    .then(categories => {
+router.delete('/deleteCategory/:categoryId', isAuth, async(request,response) => {
+    const cid = request.params.categoryId;
+    Category.findByIdAndDelete(cid)
+    .then(category_deleted => {
         return response.status(200).json({
             status: true,
-            massege: categories
-        })
-    })
-    .catch(error => {
-        return response.status(200).json({
-            status: false,
-            messege:error.message
-        })
-    })
-})
-router.get('/getCategory/:categoryId', isAuth, async(request,response) => {    
-    const categoryId = request.params.categoryId;
-    await Category.findById(categoryId)
-    .then(category => {
-        response.status(200).json({
-            Categoty: category
+            message: category_deleted
         })
     })
     .catch(err => {
-        return response.status(403).json({
-            message: 'Category not found'
+        return response.status(500).json({
+            status: false,
+            message: err
         })
     })
-    
+ });
+router.post('/addProduct', isAuth, async(request,response) => { });
+router.put('/updateProduct', isAuth, async(request,response) => { });
+router.delete('/deleteProduct', isAuth, async(request,response) => { });
+router.get('/getAllCategories', isAuth, async(request,response) => {
+    const accountId = request.account._id;
+    const store = await Store.findOne({associateId: accountId});
+    Category.find({storeId: store._id})
+    .then(categories => {
+        return response.status(200).json({
+            status: true,
+            message: categories
+        });
+    })
+    .catch(err => {
+        return response.status(500).json({
+            status: false,
+            message: err
+        });
+    })
+ });
+ router.get('/getCategory/:categoryId', isAuth, async(request,response) => {
+    const cid = request.params.categoryId;
+    Category.findById(cid)
+    .then(category => {
+        return response.status(200).json({
+            status: true,
+            message: category
+        });
+    })
+    .catch(err => {
+        return response.status(500).json({
+            status: false,
+            message: err
+        });
+    })
+ });
+router.get('/getAllProducts', isAuth, async(request,response) => {
+    const accountId = request.account._id;
+    const store = await Store.findOne({associateId: accountId});
+    Product.find({storeId: store._id})
+    .then(products => {
+        return response.status(200).json({
+            status: true,
+            message: products
+        });
+    })
+    .catch(err => {
+        return response.status(500).json({
+            status: false,
+            message: err
+        });
+    })
+ });
+ router.get('/getProductsByCategoryId/:categoryId', isAuth, async(request,response) => {
+    const accountId = request.account._id;
+    const categoryId = request.params.categoryId;
+    const store = await Store.findOne({associateId: accountId});
+    Product.find({storeId: store._id, categoryId: categoryId})
+    .then(products => {
+        return response.status(200).json({
+            status: true,
+            message: products
+        });
+    })
+    .catch(err => {
+        return response.status(500).json({
+            status: false,
+            message: err
+        });
+    })
+ });
 
-})
 
 module.exports = router;
